dbe1c408b90942fe0c1a608cebe37f954974c164
filipecocolo1 <53791311+filipecocolo1@users.noreply.github.com>
2022-10-06 15:16:48 -0300

    Adjust animation movement

--- Assets/Script/MovimentPlayer.cs
+++ Assets/Script/MovimentPlayer.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class MovimentPlayer : MonoBehaviour
 {
     private Rigidbody2D RB;
     public float _pSpeed;
     private Vector2 _pDirection;
+    public Animator _animator;
 
     void Start()
     {
@@ -15,8 +17,17 @@ public class MovimentPlayer : MonoBehaviour
 
     void Update()
     {
-        _pDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        _pDirection.x = (Input.GetAxisRaw("Horizontal"));
+        _pDirection.y = (Input.GetAxisRaw("Vertical"));
+
+        _animator.SetFloat("Horizontal", _pDirection.x);
+        _animator.SetFloat("Vertical", _pDirection.y);
+        _animator.SetFloat("Velocidade",_pDirection.sqrMagnitude);
+
+
+
     }
+
     void FixedUpdate()
     {
         RB.MovePosition(RB.position +_pDirection *_pSpeed *Time.fixedDeltaTime);
